e6fecb80bfab7cb63efb952b0564180f7e767c1d
xnpelur <michael.s5102@gmail.com>
2025-05-28 10:32:58 +0300

    Code examples text on landing

--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -1,34 +1,30 @@
 import { createElement, VirtualNode } from "@/runtime/jsx-runtime";
 import { CodeDemo } from "@/app/components/demo/code-demo";
-import { useTranslation } from "@/runtime/hooks/translation";
 
 export function Section({
+    title,
+    firstDescription,
+    secondDescription,
     codeTitle,
     CodeCardComponent,
     CodeResultComponent,
 }: {
+    title: string;
+    firstDescription: string;
+    secondDescription: string;
     codeTitle: string;
     CodeCardComponent: () => VirtualNode;
     CodeResultComponent: () => VirtualNode;
 }) {
-    const { t } = useTranslation();
     return (
         <div class="mx-auto flex flex-col w-full bg-gradient-left dark:bg-gradient-left-dark border-t border-gray-900/10 dark:border-gray-50/10">
             <div class="flex-col gap-2 flex grow w-full my-32 mx-auto items-center">
                 <div class="px-0 max-w-4xl text-center text-white flex flex-col items-center justify-center">
                     <h2 class="leading-xl font-display text-gray-950 dark:text-gray-50 font-semibold text-6xl -mt-4 mb-7 w-full max-w-xl">
-                        {
-                            t(
-                                "section.heading.createUI"
-                            ) /* Create user interfaces from components */
-                        }
+                        {title}
                     </h2>
                     <p class="max-w-3xl mx-auto text-xl text-gray-700 dark:text-gray-100 leading-normal">
-                        {
-                            t(
-                                "section.text.coreactLets"
-                            ) /* Coreact lets you build user interfaces out of individual pieces called components. Create your own Coreact components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps. */
-                        }
+                        {firstDescription}
                     </p>
                 </div>
                 <div class="max-w-7xl mx-auto flex flex-col w-full">
@@ -44,11 +40,7 @@ export function Section({
                 </div>
                 <div class="px-0 max-w-4xl text-center text-white flex flex-col items-center justify-center">
                     <p class="max-w-3xl mx-auto text-xl text-gray-700 dark:text-gray-100 leading-normal">
-                        {
-                            t(
-                                "section.text.whetherYou"
-                            ) /* Whether you work on your own or with thousands of other developers, using Coreact feels the same. It is designed to let you seamlessly combine components written by independent people, teams, and organizations. */
-                        }
+                        {secondDescription}
                     </p>
                 </div>
             </div>
--- src/app/pages/home-page.tsx
+++ src/app/pages/home-page.tsx
@@ -10,8 +10,10 @@ import { EffectCode } from "@/app/components/examples/effect-code";
 import { EffectResult } from "@/app/components/examples/effect-result";
 import { StoreCode } from "@/app/components/examples/store-code";
 import { StoreResult } from "@/app/components/examples/store-result";
+import { useTranslation } from "@/runtime/hooks/translation";
 
 export function HomePage(): VirtualNode {
+    const { t } = useTranslation();
     return (
         <div class="font-text antialiased text-lg bg-white dark:bg-darkgray text-gray-700 dark:text-gray-100 leading-base">
             <Navbar />
@@ -20,16 +22,37 @@ export function HomePage(): VirtualNode {
                     <div class="pl-0">
                         <Hero />
                         <Section
+                            title={t("section.state.title")}
+                            firstDescription={t(
+                                "section.state.firstDescription"
+                            )}
+                            secondDescription={t(
+                                "section.state.secondDescription"
+                            )}
                             codeTitle="State"
                             CodeCardComponent={StateCode}
                             CodeResultComponent={StateResult}
                         />
                         <Section
+                            title={t("section.effect.title")}
+                            firstDescription={t(
+                                "section.effect.firstDescription"
+                            )}
+                            secondDescription={t(
+                                "section.effect.secondDescription"
+                            )}
                             codeTitle="Effect"
                             CodeCardComponent={EffectCode}
                             CodeResultComponent={EffectResult}
                         />
                         <Section
+                            title={t("section.store.title")}
+                            firstDescription={t(
+                                "section.store.firstDescription"
+                            )}
+                            secondDescription={t(
+                                "section.store.secondDescription"
+                            )}
                             codeTitle="Store"
                             CodeCardComponent={StoreCode}
                             CodeResultComponent={StoreResult}
